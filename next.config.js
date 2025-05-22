a0a969f95007acd75c1a8b70acb68c90bdd8923b
keshav165 <keshavreddyv@gmail.com>
2025-05-23 01:52:19 +0530

    Update CSP configuration for Spline compatibility

--- next.config.js
+++ next.config.js
@@ -13,10 +13,12 @@ const nextConfig = {
       'www.gstatic.com',
       'spline.design',
       's3-alpha.figma.com',
-      'spline-3d.s3.us-west-2.amazonaws.com'
+      'spline-3d.s3.us-west-2.amazonaws.com',
     ],
   },
+
   webpack: (config, { isServer }) => {
+    // Enable WebAssembly support
     config.experiments = {
       ...config.experiments,
       asyncWebAssembly: true,
@@ -28,9 +30,11 @@ const nextConfig = {
 
     return config;
   },
+
   experimental: {
     esmExternals: false,
   },
+
   async headers() {
     return [
       {
@@ -40,6 +44,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: isDev
               ? [
+                  // Development CSP: allow inline scripts and eval for ease of dev
                   `default-src 'self' data: blob: https:;`,
                   `script-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: https:;`,
                   `style-src 'self' 'unsafe-inline' https:;`,
@@ -58,28 +63,54 @@ const nextConfig = {
                   `upgrade-insecure-requests;`,
                 ].join(' ')
               : [
+                  // Production CSP: strict hashes + trusted domains, no unsafe-inline
                   `default-src 'self' data: blob: https:;`,
-                  `script-src 'self' 'sha256-c5Gm1Iytmxlbm7sLzBSUu/dpFv9EWweQTfDc8mG3PME=' 'sha256-Q+8tPsjVtiDsjF/Cv8FMOpg2Yg91oKFKDAJat1PPb2g=' 'sha256-yv/MvfuhaaGjfZSG16fGtAsqb2kN9yMlLNJIHvfHK1I=' 'sha256-Cbd+ntPmFzSXjhRvTIEhSP8CkB5ynH283nhlzUDGiFs=' 'sha256-Ce4/Ph//Ghv71MQk5ifG8L+QoBacoqd2F3Gb0BoIGoc=' 'sha256-L9PhcDR1JpjxjPsZVTF9m718jhSRvj3mNXP6/Xh718w=' 'sha256-rU8KluWUOzsbf7dwvdj/cRJ1P7+RbvNdSANw93Fgflg=' https://prod.spline.design https://unpkg.com https://spline.design;`,
+
+                  // List of script hashes and allowed script domains
+                  `script-src 'self' ` +
+                    `'sha256-c5Gm1Iytmxlbm7sLzBSUu/dpFv9EWweQTfDc8mG3PME=' ` +
+                    `'sha256-Q+8tPsjVtiDsjF/Cv8FMOpg2Yg91oKFKDAJat1PPb2g=' ` +
+                    `'sha256-yv/MvfuhaaGjfZSG16fGtAsqb2kN9yMlLNJIHvfHK1I=' ` +
+                    `'sha256-Cbd+ntPmFzSXjhRvTIEhSP8CkB5ynH283nhlzUDGiFs=' ` +
+                    `'sha256-Ce4/Ph//Ghv71MQk5ifG8L+QoBacoqd2F3Gb0BoIGoc=' ` +
+                    `'sha256-L9PhcDR1JpjxjPsZVTF9m718jhSRvj3mNXP6/Xh718w=' ` +
+                    `'sha256-rU8KluWUOzsbf7dwvdj/cRJ1P7+RbvNdSANw93Fgflg=' ` +
+                    `https://prod.spline.design https://unpkg.com https://spline.design;`,
+
                   `style-src 'self' 'unsafe-inline' https: data:;`,
+
+                  // Image sources allowed
                   `img-src 'self' data: blob: https: https://prod.spline.design https://unpkg.com https://spline.design https://s3-alpha.figma.com https://*.spline.design https://spline-3d.s3.us-west-2.amazonaws.com;`,
+
                   `connect-src 'self' https: wss: data: blob: https://prod.spline.design https://unpkg.com https://spline.design https://s3-alpha.figma.com https://*.spline.design https://spline-3d.s3.us-west-2.amazonaws.com;`,
+
                   `frame-src 'self' https: data: https://prod.spline.design https://unpkg.com https://spline.design;`,
+
                   `worker-src 'self' blob: data: https://prod.spline.design https://unpkg.com https://spline.design;`,
+
                   `media-src 'self' data: blob: https://prod.spline.design https://unpkg.com https://spline.design;`,
+
                   `font-src 'self' https: data:;`,
+
                   `manifest-src 'self' https:;`,
+
                   `object-src 'none';`,
+
                   `base-uri 'self';`,
+
                   `form-action 'self';`,
+
                   `frame-ancestors 'none';`,
+
                   `block-all-mixed-content;`,
+
                   `upgrade-insecure-requests;`,
-                ].join(' ')
-          }
-        ]
-      }
+                ].join(' '),
+          },
+        ],
+      },
     ];
-  }
+  },
 };
 
 module.exports = nextConfig;
